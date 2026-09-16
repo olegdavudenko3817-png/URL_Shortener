4d5fe3066f0fe0b4d57318282abe57599aa440cc
olegdavudenko3817 <oleg.davudenko3817@gmail.com>
2026-09-16 21:14:20 +0300

    Fix CI test

--- src/test/java/com/example/url_shortener/AppLauncherTests.java
+++ src/test/java/com/example/url_shortener/AppLauncherTests.java
@@ -3,12 +3,17 @@ package com.example.url_shortener;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@TestPropertySource(properties = {
+        "JWT_SECRET=this-is-a-test-secret-key-with-enough-length-123456",
+        "JWT_EXPIRATION=86400000"
+})
 class AppLauncherTests {
     @Container
     @ServiceConnection
